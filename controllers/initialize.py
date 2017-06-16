944a596d6009ed6d39d3deb90c0e2133da9d4c0a
Pablo Delgado <poldelgado@gmail.com>
2017-06-16 17:38:12 -0300

    fixed initialize controller

--- controllers/initialize.py
+++ controllers/initialize.py
@@ -13,7 +13,6 @@ def adminuser():
             email=myconf.get('admin_user.email'),
             first_name=myconf.get('admin_user.first_name'),
             last_name=myconf.get('admin_user.last_name'),
-            ssn=myconf.get('admin_user.ssn'),
         )
 
         user = auth.login_bare(
