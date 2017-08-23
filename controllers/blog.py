1f1b53c7152841191efbe80107afe841d2d7db01
Pablo Delgado <poldelgado@gmail.com>
2017-08-23 19:59:28 -0300

    fixed accordion show, sidebar blog changed

--- controllers/blog.py
+++ controllers/blog.py
@@ -16,12 +16,13 @@ def show():
 	return dict(post = post, categories = categories)
 
 def show_by_category():
-	if not request.vars.page:
-		redirect(URL(vars={'page':1}))
-	else:
-		page = int(request.vars.page)
-	start = (page-1)*5
-	end = page*5
+	# if not request.vars.page:
+	# 	redirect(URL(vars={'page':1}))
+	# else:
+	# 	page = int(request.vars.page)
+	# start = (page-1)*5
+	# end = page*5
 	posts = db(db.post.category == request.args(0)).select() or redirect(URL('index'))
+	latestpost = db(db.post).select(orderby=~db.post.date_of_post, limitby=(0,10))
 	categories = db(db.post_category).select().sort(lambda post_category: len(post_category.name)) #post category sorted by name size
-	return dict(posts=posts, categories = categories)
+	return dict(posts=posts, categories = categories, latestpost = latestpost)
