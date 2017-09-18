6a57131036394391ffa767bb47437a8dceec9c39
Pablo Delgado <poldelgado@gmail.com>
2017-09-18 12:03:22 -0300

    graduated updated

--- controllers/graduated.py
+++ controllers/graduated.py
@@ -1,4 +1,5 @@
 def index():
+	graduation = db().select(db.graduation.ALL, orderby=db.graduation.number|db.graduation.last_name|db.graduation.first_name)
 	categories = db(db.post_category).select().sort(lambda post_category: len(post_category.name)) #post category sorted by name size
-	return dict(categories = categories)
+	return dict(categories = categories, graduation = graduation)
 
---
+++ controllers/history.py
@@ -0,0 +1,3 @@
+def index():
+	categories = db(db.post_category).select().sort(lambda post_category: len(post_category.name)) #post category sorted by name size
+	return dict(categories = categories)
